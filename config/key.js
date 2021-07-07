41402621885d1bdfff12789aa42d8ae3c5226665
Vương Vĩnh An <vuongvinhan98@gmail.com>
2021-07-07 14:09:43 +0700

    new

--- config/key.js
+++ config/key.js
@@ -4,7 +4,7 @@
 module.exports = {
     googleClientID: '1019976649583-8ne40k7ivoqv85uqcvb4jo5aj23770uc.apps.googleusercontent.com',
     googleClientSecret: 'BhA5PjM3yRSwtQYTKOv5x51E',
-    mongoURI: 'mongodb+srv://online-course:onlineCourse@online-course.vptky.mongodb.net/online-course?retryWrites=true&w=majority',
+    mongoURI: 'mongodb+srv://admin:Google12358@online-course.vptky.mongodb.net/online-course?retryWrites=true&w=majority',
     cookieKey: 'asdsadsadsadewrtegfbvcngfjhytduqawezd'
 
 };
